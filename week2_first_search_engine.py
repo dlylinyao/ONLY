84291298487d2a2ea50072ad61cca5aae6d13e90
oonaitko <oonaitko@Oona-MacBook-Pro.local>
2026-01-21 14:08:53 +0200

    #12 boolean query

--- week2_first_search_engine.py
+++ week2_first_search_engine.py
@@ -36,6 +36,24 @@ def get_week1_documents():
         print(f"An unexpected error occurred: {e}")
         return []
 
+#Issue 12, performing a query with a parser. This is mostly copied from the tutorial once again.
+
+d = {"and": "&", "AND": "&",
+     "or": "|", "OR": "|",
+     "not": "1 -", "NOT": "1 -",
+     "(": "(", ")": ")"}          # operator replacements
+
+def rewrite_token(t):
+    return d.get(t, 'sparse_td_matrix[t2i["{:s}"]].todense()'.format(t))
+
+def rewrite_query(query): # rewrite every token in the query
+    return " ".join(rewrite_token(t) for t in query.split())
+
+def test_query(query):
+    print("Query: '" + query + "'")
+    print("Rewritten:", rewrite_query(query))
+    print("Matching:", eval(rewrite_query(query))) # Eval runs the string as a Python command
+    print()
 
 # Testing Block
 # It validates that the data is ready for the search engine.
@@ -59,6 +77,7 @@ if __name__ == "__main__":
 
             sparse_matrix = cv.fit_transform(documents)
             t2i = cv.vocabulary_
+            sparse_td_matrix = sparse_matrix.T.tocsr() # to csr for bigger scale and conversion to term-document
 
             print("Vectorization successful!")
             print(f"Vocabulary size: {len(t2i)} unique words.")
@@ -68,3 +87,11 @@ if __name__ == "__main__":
             print(f"Vectorization failed: {e}")
     else:
         print("No documents were loaded.")
+
+    # test queries 
+
+    test_query("foreign AND NOT education")
+    test_query("NOT education OR foreign")
+    test_query("( NOT foreign OR education ) AND policy") # AND, OR, NOT can be written either in ALLCAPS
+    test_query("( not foreign or education ) and policy") # ... or all small letters
+    test_query("not foreign and not policy")   
