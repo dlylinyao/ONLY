2fe7c10dc2ef7a40ac97df41c7594120c73ee73c
Linyao Du <linyaodu@puhti-login14.bullx>
2026-03-08 18:15:01 +0200

    Update: removed Google Gemma-2-9b-it from evaluation and updated merged results to 5 models

--- launch_llm-as-a-judge.sh
+++ launch_llm-as-a-judge.sh
@@ -2,6 +2,5 @@
 sbatch llm-as-a-judge_puhti.sh Qwen/Qwen2.5-7B-Instruct
 sbatch llm-as-a-judge_puhti.sh meta-llama/Llama-3.1-8B-Instruct
 sbatch llm-as-a-judge_puhti.sh mistralai/Mistral-7B-Instruct-v0.3
-sbatch llm-as-a-judge_puhti.sh google/gemma-2-9b-it
 sbatch llm-as-a-judge_puhti.sh CohereLabs/aya-expanse-8b
 sbatch llm-as-a-judge_puhti.sh utter-project/EuroLLM-9B-Instruct
